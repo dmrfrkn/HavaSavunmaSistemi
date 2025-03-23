21f0403e7bbfc2a0ef03f50a0f5b8a627a4a0d0b
dmrfrkn <demirfurkan04@gmail.com>
2025-03-23 23:09:51 +0300

    I write a code about calculating small ballon

--- HavaSavunma/Nesne Takip Görev 1&2/png_size_detect.py
+++ HavaSavunma/Nesne Takip Görev 1&2/png_size_detect.py
@@ -37,36 +37,34 @@ small_baloon = None  # Küçük balonu saklayacak değişken
 for contour in contours_blue:
     if cv2.contourArea(contour) > 500:  # Küçük gürültüleri engelle
         x, y, w, h = cv2.boundingRect(contour)
-        # Alanı hesaplama
-        area = w * h
-        print(f"Blue Area: {area} px")  # Alanı konsola yazdır
         
-        # Küçük balonu belirleme
-        if small_baloon is None or area < small_baloon[0]:  # Eğer küçük balon yoksa veya bu alan daha küçükse
-            small_baloon = (area, "Blue")  # Alanı ve rengini tut
+        area = w * h
+        print(f"Blue Area: {area} px") 
+        
+        if small_baloon is None or area < small_baloon[0]:  
+            small_baloon = (area, "Blue") 
 
-        # Dikdörtgen çizme
+        
         cv2.rectangle(image, (x, y), (x + w, y + h), (255, 0, 0), 2)
         
 for contour in contours_red:
     if cv2.contourArea(contour) > 500:
         x, y, w, h = cv2.boundingRect(contour)
-        # Alanı hesaplama
-        area = w * h
-        print(f"Red Area: {area} px")  # Alanı konsola yazdır
         
-        # Küçük balonu belirleme
-        if small_baloon is None or area < small_baloon[0]:  # Eğer küçük balon yoksa veya bu alan daha küçükse
-            small_baloon = (area, "Red")  # Alanı ve rengini tut
+        area = w * h
+        print(f"Red Area: {area} px") 
+    
+        if small_baloon is None or area < small_baloon[0]:  
+            small_baloon = (area, "Red")  
 
-        # Dikdörtgen çizme
+      
         cv2.rectangle(image, (x, y), (x + w, y + h), (0, 0, 255), 2)
 
-# Küçük balonu konsola yazdırma
+
 if small_baloon:
     print(f"Small Baloon: {small_baloon[1]} baloon with Area: {small_baloon[0]} px")
 
-# Sonucu göster
+
 cv2.imshow("Balon Algilama", image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
